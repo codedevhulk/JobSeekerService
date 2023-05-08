cc30ed9d89116d59823b6873996f77667b08eb76
seshu <18pa1a0202@vishnu.edu.in>
2023-05-08 16:17:54 +0530

    Added default packages of JobSeekerService

---
+++ JobSeekerService/src/main/java/com/JobSeeker/JobSeekerService/JobSeekerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.JobSeeker.JobSeekerService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobSeekerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobSeekerServiceApplication.class, args);
+	}
+
+}
---
+++ JobSeekerService/src/test/java/com/JobSeeker/JobSeekerService/JobSeekerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.JobSeeker.JobSeekerService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobSeekerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
