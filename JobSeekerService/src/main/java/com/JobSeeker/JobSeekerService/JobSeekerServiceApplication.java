161200f1ce659d05a2a97e830f4802e004cfa875
Rajesh Gottimukkula <rajesh.gottimukkula1@gmail.com>
2023-06-15 11:24:37 +0530

    Latest

--- JobSeekerService/src/main/java/com/JobSeeker/JobSeekerService/JobSeekerServiceApplication.java
+++ JobSeekerService/src/main/java/com/JobSeeker/JobSeekerService/JobSeekerServiceApplication.java
@@ -2,13 +2,15 @@ package com.JobSeeker.JobSeekerService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
+@SpringBootApplication
 public class JobSeekerServiceApplication {
-
+//(exclude = SecurityAutoConfiguration.class)
 	public static void main(String[] args) {
 		SpringApplication.run(JobSeekerServiceApplication.class, args);
 	}
 
 }
+//(exclude = {SecurityAutoConfiguration.class})
