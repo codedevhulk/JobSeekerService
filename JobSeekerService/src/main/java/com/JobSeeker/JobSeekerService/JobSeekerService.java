9b10ea1b3bd103224cec6cdace66ccdda1bab800
Rajesh Gottimukkula <rajesh.gottimukkula1@gmail.com>
2023-07-07 10:34:52 +0530

    latest

--- JobSeekerService/src/main/java/com/JobSeeker/JobSeekerService/JobSeekerService.java
+++ JobSeekerService/src/main/java/com/JobSeeker/JobSeekerService/JobSeekerService.java
@@ -20,87 +20,76 @@ import lombok.extern.slf4j.Slf4j;
 public class JobSeekerService {
 	@Autowired
 	PasswordEncoder passwordEncoder;
-    @Autowired
-    JobSeekerRepository repo;
+	@Autowired
+	JobSeekerRepository repo;
+
 	public void signupDetails(JobSeekerDetails jobSeekerDetailsRequest) {
-		
-		
-		if(jobSeekerDetailsRequest.getUserName().isEmpty()) {
-			
-			
-			throw new CustomException("User name is mandatory for signup, please provide the user name","BAD_REQUEST",400);
+
+		if (jobSeekerDetailsRequest.getUserName().isEmpty()) {
+
+			throw new CustomException("User name is mandatory for signup, please provide the user name", "BAD_REQUEST",
+					400);
 		}
+
+		log.info("Checking whether job seeker details details exists");
 		
-		JobSeekerDetails jobSeekerDetails=repo.findByUserName(jobSeekerDetailsRequest.getUserName());
-		
-		//log.info(jobSeekerDetails.toString());
-		
-		//updating the Jobseekerdetails
-		
-		
-		if(Objects.nonNull(jobSeekerDetails)) {
-			
-			
-			
-			if(Objects.nonNull(jobSeekerDetails.getJobSeekerId())) {
-				
-				
-				
+		JobSeekerDetails jobSeekerDetails = repo.findByUserName(jobSeekerDetailsRequest.getUserName());
+
+		// log.info(jobSeekerDetails.toString());
+
+		// updating the Jobseekerdetails
+
+		if (Objects.nonNull(jobSeekerDetails)) {
+
+			if (Objects.nonNull(jobSeekerDetails.getJobSeekerId())) {
+
 				jobSeekerDetails.setJobSeekerId(jobSeekerDetailsRequest.getJobSeekerId());
 			}
-			
-			
-			
-			
-			if(Objects.nonNull(jobSeekerDetailsRequest.getFirstName())) {
+
+			if (Objects.nonNull(jobSeekerDetailsRequest.getFirstName())) {
 				jobSeekerDetails.setFirstName(jobSeekerDetailsRequest.getFirstName());
 			}
-			if(Objects.nonNull(jobSeekerDetailsRequest.getLastName())) {
+			if (Objects.nonNull(jobSeekerDetailsRequest.getLastName())) {
 				jobSeekerDetails.setLastName(jobSeekerDetailsRequest.getLastName());
 			}
-			if(Objects.nonNull(jobSeekerDetailsRequest.getUserName())) {
+			if (Objects.nonNull(jobSeekerDetailsRequest.getUserName())) {
 				jobSeekerDetails.setUserName(jobSeekerDetailsRequest.getUserName());
 			}
-			if(Objects.nonNull(jobSeekerDetailsRequest.getMobileNumber())) {
+			if (Objects.nonNull(jobSeekerDetailsRequest.getMobileNumber())) {
 				jobSeekerDetails.setMobileNumber(jobSeekerDetailsRequest.getMobileNumber());
 			}
-			if(Objects.nonNull(jobSeekerDetailsRequest.getEmail())) {
+			if (Objects.nonNull(jobSeekerDetailsRequest.getEmail())) {
 				jobSeekerDetails.setEmail(jobSeekerDetailsRequest.getEmail());
 			}
-			if(Objects.nonNull(jobSeekerDetailsRequest.getQualification())) {
+			if (Objects.nonNull(jobSeekerDetailsRequest.getQualification())) {
 				jobSeekerDetails.setQualification(jobSeekerDetailsRequest.getQualification());
 			}
-			if(Objects.nonNull(jobSeekerDetailsRequest.getSkillSet())) {
+			if (Objects.nonNull(jobSeekerDetailsRequest.getSkillSet())) {
 				jobSeekerDetails.setSkillSet(jobSeekerDetailsRequest.getSkillSet());
 			}
-			if(Objects.nonNull(jobSeekerDetailsRequest.getExperience())) {
+			if (Objects.nonNull(jobSeekerDetailsRequest.getExperience())) {
 				jobSeekerDetails.setExperience(jobSeekerDetailsRequest.getExperience());
 			}
-			if(Objects.nonNull(jobSeekerDetailsRequest.getSummary())) {
+			if (Objects.nonNull(jobSeekerDetailsRequest.getSummary())) {
 				jobSeekerDetails.setSummary(jobSeekerDetailsRequest.getSummary());
 			}
-			if(Objects.nonNull(jobSeekerDetailsRequest.getAddress())) {
+			if (Objects.nonNull(jobSeekerDetailsRequest.getAddress())) {
 				jobSeekerDetails.setAddress(jobSeekerDetailsRequest.getAddress());
 			}
+
 			
-			
-			
-			
-			
-			
+			log.info("Saving jobseekerDetails details objects");
 			repo.save(jobSeekerDetails);
-			log.info(jobSeekerDetails.toString());
-			
+			//log.info(jobSeekerDetails.toString());
+
 		}
-		
-		
-		
+
 		// User does not exist so continuing with creating a new user
-		
+
 		else {
-		
-		
-		//String encryptedPassword = passwordEncoder.encode(jobSeekerDetailsRequest.getPassword());
+
+			// String encryptedPassword =
+			// passwordEncoder.encode(jobSeekerDetailsRequest.getPassword());
 //		JobSeekerDetails details =  JobSeekerDetails.builder()
 //				.firstName(jobSeekerDetailsRequest.getFirstName())
 //				.lastName(jobSeekerDetailsRequest.getLastName())
@@ -114,93 +103,88 @@ public class JobSeekerService {
 //				.address(jobSeekerDetailsRequest.getAddress())
 //				.build();
 //		repo.save(details);
-		//jobSeekerDetailsRequest.setPassword(encryptedPassword);
-		repo.save(jobSeekerDetailsRequest);
-		
-		log.info(jobSeekerDetailsRequest.toString());
-		//repo.save(null)
+			// jobSeekerDetailsRequest.setPassword(encryptedPassword);
+			repo.save(jobSeekerDetailsRequest);
+
+			log.info(jobSeekerDetailsRequest.toString());
+			// repo.save(null)
+		}
+
 	}
-		
-	}
+
 	public List<JobSeekerDetails> allDetails() {
+		
+		
+		
+		log.info("Fetching all the job seeker details from Repo");
 		return repo.findAll();
 	}
-	
-	
-	
-	
-	
-	
+
 	public String signInDetails(SignInDetailsRequest signInDetailsRequest) {
-		String message="";
-		JobSeekerDetails signinDetails = repo.findByEmail(signInDetailsRequest.getEmail());
-		if (signinDetails!= null) 
-		{
-		 String password = signInDetailsRequest.getPassword();
-		 String encodedPassword = signinDetails.getPassword();
-         Boolean isPwdRight = passwordEncoder.matches(password, encodedPassword);
-        if (isPwdRight) 
-        {
-        	
-        	
-		    message="Login Success";
-		    return message;
-
-		} else 
-		{
-			message = "Login Failed";
-			return message;
-	     }
-
-		}
-		else {
-            message="Email not exits";
-            return message;
-        }
+		String message = "";
 		
+		
+		JobSeekerDetails signinDetails = repo.findByEmail(signInDetailsRequest.getEmail());
+		if (signinDetails != null) {
+			String password = signInDetailsRequest.getPassword();
+			String encodedPassword = signinDetails.getPassword();
+			Boolean isPwdRight = passwordEncoder.matches(password, encodedPassword);
+			if (isPwdRight) {
+
+				message = "Login Success";
+				return message;
+
+			} else {
+				message = "Login Failed";
+				return message;
+			}
+
+		} else {
+			message = "Email not exits";
+			return message;
+		}
+
 	}
+
 	public JobSeekerDetails getJobSeekerById(long id) {
 		// TODO Auto-generated method stub
-		
-		JobSeekerDetails jobSeekerDetails= repo.findByJobSeekerId(id);
-		if(Objects.isNull(jobSeekerDetails)) {
-			throw new CustomException("Job seeker not found with ID: "+id,"NOT_FOUND",404);
+		log.info("Getting the job seeker by ID " + id);
+		JobSeekerDetails jobSeekerDetails = repo.findByJobSeekerId(id);
+		if (Objects.isNull(jobSeekerDetails)) {
+			throw new CustomException("Job seeker not found with ID: " + id, "NOT_FOUND", 404);
+		} else {
+			return jobSeekerDetails;
 		}
-		else {
-		return jobSeekerDetails;}
-		
-		
-		
+
 	}
+
 	public JobSeekerDetails getJobSeekerByUserName(String userName) {
 		// TODO Auto-generated method stub
-		log.info("user name"+userName);
-		
-		
-		JobSeekerDetails jobSeekerDetailsByName=repo.findByUserName(userName);
-		if(Objects.isNull(jobSeekerDetailsByName)) {
-			
-			
-			
-			
-			throw new CustomException("Job seeker doesn't found with name: "+userName,"NOT_FOUND",404);
-		}
-		else {
+		log.info("Getting the job seeker details by user name" + userName);
+
+		JobSeekerDetails jobSeekerDetailsByName = repo.findByUserName(userName);
+		if (Objects.isNull(jobSeekerDetailsByName)) {
+
+			throw new CustomException("Job seeker doesn't found with name: " + userName, "NOT_FOUND", 404);
+		} else {
 			return jobSeekerDetailsByName;
 		}
 	}
+
 	public void deleteUserByUserName(String userName) {
-		
-		JobSeekerDetails jobSeekerDetailsByName=repo.findByUserName(userName);
-		if(Objects.isNull(jobSeekerDetailsByName)) {
-			
-			throw new CustomException("Job seeker doesn't found with name: "+userName,"NOT_FOUND",404);
+
+		log.info("Finding the user with name " + userName + " , If not found throwing an error");
+
+		JobSeekerDetails jobSeekerDetailsByName = repo.findByUserName(userName);
+		if (Objects.isNull(jobSeekerDetailsByName)) {
+
+			throw new CustomException("Job seeker doesn't found with name: " + userName, "NOT_FOUND", 404);
 		}
 		// TODO Auto-generated method stub
+		log.info("Deleting the user with name " + userName);
+
 		repo.deleteByUserName(userName);
-		
+
 	}
-	
-	
 
 }
